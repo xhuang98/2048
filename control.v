dcf7e2b5523bdb3bfc6bcc055b1b298c62aa5664
huang632 <xibill.huang@mail.utoronto.ca>
2018-03-25 16:37:27 -0400

    need to add keyboard

--- control.v
+++ control.v
@@ -1,8 +1,9 @@
-module control(start, clock, up, down, right, left, update, values);
+module control(start, clock, up, down, right, left, oldvalues, update, newvalues);
 	input start, clock, up, down, right, left;
+	input [16 * 4 - 1 : 0] newvalues; //new values for all boxes
 	output update; // is the enable for the boxes to load new values
-	output [16 * 4 : 0] values; //new values for all boxes
-	
+	output [16 * 4 - 1 : 0] oldvalues; //old values from all boxes
+
 	// TODO: Different states
 	// TODO: actions according to states
 endmodule
