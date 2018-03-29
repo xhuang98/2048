946d26d17ce049f06c24fee69b9dc735cfb7e5c3
amjadkar <37787272+amjadkar@users.noreply.github.com>
2018-03-28 21:01:00 -0400

    variable counter

--- variable_counter.v
+++ variable_counter.v
@@ -1,12 +1,19 @@
-module variable_counter(reset, CLOCK_50, outvalue);
+module variable_counter(reset, CLOCK_50, outvalue, outvalue2);
     input reset;
     input CLOCK_50;
     output [3:0] outvalue;
+    output [3:0] outvalue2;
     t_ff T0( //toggle flip flops
        .enable(1'b1), 
        .clock(CLOCK_50), 
        .reset(reset),
        .q(outvalue)
+       );
+    t_ffe T1( //toggle flip flops
+       .enable(1'b1), 
+       .clock(CLOCK_50), 
+       .reset(reset),
+       .q(outvalue2)
        );
 endmodule
 
@@ -24,5 +31,22 @@ module t_ff(enable, clock, reset, q);
             q <= 0;
         else if(enable == 1'b1) //counts upwards for hex value
             q <= q + 1;
+    end
+endmodule
+
+module t_ffe(enable, clock, reset, q);
+    input enable, clock, reset;
+    output reg [3:0] q;
+
+    always @(posedge clock)
+    begin
+        if(reset == 1'b1) //reset when high
+        begin
+            q <= 0;
+        end
+        else if(q > 4'b1111) //when hex display reaches F reset
+            q <= 0;
+        else if(enable == 1'b1) //counts upwards for hex value
+            q <= q + 2;
     end
 endmodule
