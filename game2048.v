a3bfb51b72aba22bc617f8382db361c6cf57c782
amjadkar <37787272+amjadkar@users.noreply.github.com>
2018-03-29 13:52:55 -0400

    Add files via upload
    Added details

--- game2048.v
+++ game2048.v
@@ -69,21 +69,21 @@ module game2048(
 	
 	// start is reset
 	box b1(box1in, enable, start, clock, box1out);
-		b2(box2in, enable, start, clock, box2out);
-		b3(box3in, enable, start, clock, box3out);
-		b4(box4in, enable, start, clock, box4out);
-		b5(box5in, enable, start, clock, box5out);
-		b6(box6in, enable, start, clock, box6out);
-		b7(box7in, enable, start, clock, box7out);
-		b8(box8in, enable, start, clock, box8out);
-		b9(box9in, enable, start, clock, box9out);
-		b10(box10in, enable, start, clock, box10out);
-		b11(box11in, enable, start, clock, box11out);
-		b12(box12in, enable, start, clock, box12out);
-		b13(box13in, enable, start, clock, box13out);
-		b14(box14in, enable, start, clock, box14out);
-		b15(box15in, enable, start, clock, box15out);
-		b16(box16in, enable, start, clock, box16out);
+	box	b2(box2in, enable, start, clock, box2out);
+	box	b3(box3in, enable, start, clock, box3out);
+	box	b4(box4in, enable, start, clock, box4out);
+	box	b5(box5in, enable, start, clock, box5out);
+	box	b6(box6in, enable, start, clock, box6out);
+	box	b7(box7in, enable, start, clock, box7out);
+	box	b8(box8in, enable, start, clock, box8out);
+	box	b9(box9in, enable, start, clock, box9out);
+	box	b10(box10in, enable, start, clock, box10out);
+	box	b11(box11in, enable, start, clock, box11out);
+	box	b12(box12in, enable, start, clock, box12out);
+	box	b13(box13in, enable, start, clock, box13out);
+	box	b14(box14in, enable, start, clock, box14out);
+	box	b15(box15in, enable, start, clock, box15out);
+	box	b16(box16in, enable, start, clock, box16out);
 		
 	assign {box1in, box2in, box3in, box4in, box5in, box6in, box7in, box8in, box9in, box10in, box11in, box12in, box13in, box14in, box15in, box16in} = newvalues;
 	assign oldvalues = {box1out, box2out, box3out, box4out, box5out, box6out, box7out, box8out, box9out, box10out, box11out, box12out, box13out, box14out, box15out, box16out};
